7230c1fde9b06db7bf4face238976db96a1ba51d
Tanveer <ahmedtanveer0411@gmail.com>
2021-09-15 20:06:53 +0530

    Semantic UI page design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-angular-project';
+  title: String = 'Employees of the Year 2021';
+
+  employees: Employee[] = [
+    { id: '1', name: 'Sandeep', salary: 25000, status: 'Paid' },
+    { id: '2', name: 'Mark', salary: 25500, status: 'Received' },
+    { id: '3', name: 'John', salary: 25700, status: 'Paid' },
+    { id: '4', name: 'Sanjay', salary: 25900, status: 'Not Paid' },
+  ];
 }
